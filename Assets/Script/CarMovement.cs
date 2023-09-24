cb9fcaee7c2aa5df22cf500eca7f34e9ab4de44d
Md.Razib Hossain <123752824+Razib-R75@users.noreply.github.com>
2023-09-24 13:51:44 +0600

    file
    Driving controll update

--- Assets/Script/CarMovement.cs
+++ Assets/Script/CarMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CarMovement : MonoBehaviour
 {
     [SerializeField] float MoveSpeed = 20.0f;
+    public float trunSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,10 @@ public class CarMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward*Time.deltaTime*MoveSpeed);
+        float Horizental = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * MoveSpeed * Vertical);
+
+        transform.Rotate(Vector3.up, Time.deltaTime * trunSpeed * Horizental);
     }
 }
