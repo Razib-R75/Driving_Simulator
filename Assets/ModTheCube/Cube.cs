a7f982c6cd2814312a50c98a654d3d75eaee4057
Md.Razib Hossain <123752824+Razib-R75@users.noreply.github.com>
2023-10-01 20:21:40 +0600

    file
    ..

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+        transform.localScale = Vector3.one * 1.3f;
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
